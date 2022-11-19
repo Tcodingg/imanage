f020f8d1e05b3d68f8a567df005c03f9834247e1
Tcodingg <princetlive@yahoo.com>
2022-11-19 16:36:27 -0500

    style header icons

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,11 +1,13 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import { AiOutlineLogin } from 'react-icons/ai';
+import { BsEnvelope, BsBell } from 'react-icons/bs';
 
 const Header = () => {
   return (
     <header className='max-w-4xl m-auto py-2 flex items-center justify-between'>
-      <div className='flex items-center gap-3 text-2xl'>
+      <Link href={'/'} className='flex items-center gap-4 text-2xl'>
         <Image
           src={'/assets/images/logo.png'}
           width={40}
@@ -13,10 +15,12 @@ const Header = () => {
           alt='abc'
         />
         <span className='flex font-semibold tracking-widest'>iManage</span>
-      </div>
+      </Link>
       <nav>
-        <ul>
-          <Icons child={<AiOutlineLogin />} />
+        <ul className='flex gap-4'>
+          <IconContainer icon={<BsBell />} link='/' />
+          <IconContainer icon={<BsEnvelope />} link='#' />
+          <IconContainer icon={<AiOutlineLogin link='#' />} />
         </ul>
       </nav>
     </header>
@@ -25,8 +29,8 @@ const Header = () => {
 
 export default Header;
 
-const Icons = ({ child }) => (
-  <li className='text-2xl text-gray-400 hover:cursor-pointer hover:bg-gray-200 p-1 rounded-md transition-all ease-in-out duration-300'>
-    {child}
+const IconContainer = ({ icon, link }) => (
+  <li className='text-2xl text-gray-400 hover:cursor-pointer font-extralight hover:bg-gray-200 p-2 rounded-md transition-all ease-in duration-300'>
+    <Link href={link || '#'}>{icon}</Link>
   </li>
 );
