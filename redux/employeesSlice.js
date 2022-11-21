07bfe58e502e658186adde86c40e909026a6a8d9
Tcodingg <princetlive@yahoo.com>
2022-11-21 16:38:48 -0500

    replace the depreciated extraReducers with builder

--- redux/employeesSlice.js
+++ redux/employeesSlice.js
@@ -27,18 +27,20 @@ const employeesSlice = createSlice({
       state.employees.push(action.payload);
     },
   },
-  extraReducers: {
-    [getEmployees.pending]: (state, action) => {
-      state.isLoading = true;
-    },
-    [getEmployees.fulfilled]: (state, action) => {
-      state.isLoading = false;
-      state.employees = action.payload;
-    },
-    [getEmployees.rejected]: (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(getEmployees.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getEmployees.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.employees = action.payload;
+      })
+      .addCase(getEmployees.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
   },
 });
 
