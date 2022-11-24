be56d3d39438471ba2277c5f3e7b7ef40b4b1a08
Tcodingg <princetlive@yahoo.com>
2022-11-23 21:40:03 -0500

    add type employee, salary and status input fields

--- pages/edit.js
+++ pages/edit.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { RiDeleteBinLine } from 'react-icons/ri';
 
 const Edit = () => {
+  const selectClass =
+    'form-select font-sans capitalize appearance-none block w-full px-3 py-3 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 transition-all ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:outline-none';
   return (
-    <section className='max-w-4xl m-auto px-6'>
+    <section className='max-w-4xl m-auto px-6 py-6'>
       <h3 className='capitalize font-medium pt-6'>Edit Profile</h3>
       <div>
         <div className='flex items-center '>
@@ -18,9 +20,6 @@ const Edit = () => {
               className='border p-3 w-40 text-center flex justify-center items-center
             gap-4'
             >
-              <span className='text-red-600 text-lg'>
-                <RiDeleteBinLine />
-              </span>
               Delete
             </button>
           </div>
@@ -29,13 +28,47 @@ const Edit = () => {
           Recommended size is 256 X 256 or leave it blank.
         </p>
       </div>
-      <form className='mt-10 flex flex-col'>
-        <Label label={'name'} />
-        <input
-          className='border text-base px-2 py-3 max-w-[340px] '
-          type='text'
-        />
+      <form className='mt-10 flex flex-col gap-6'>
+        <FormsContainer>
+          <Label label={'name'} />
+          <input className='border text-base px-2 py-3  ' type='text' />
+        </FormsContainer>
+
+        <FormsContainer>
+          <Label label={'role'} />
+          <select className={selectClass} name='' id=''>
+            <Option value={'software engineer'} />
+            <Option value={'executive assistants'} />
+            <Option value={'accountant'} />
+            <Option value={'Administrative assistant'} />
+          </select>
+        </FormsContainer>
+        <FormsContainer>
+          <Label label={'type employee'} />
+          <select className={selectClass} name='' id=''>
+            <Option value={'Full Time'} />
+            <Option value={'Part Time'} />
+          </select>
+        </FormsContainer>
+        <FormsContainer>
+          <Label label={'salary'} />
+          <input
+            className='border text-base px-2 py-3 appearance-none'
+            type='text'
+          />
+        </FormsContainer>
+
+        <FormsContainer>
+          <Label label={'status'} />
+          <select className={selectClass} name='' id=''>
+            <Option value={'Active'} />
+            <Option value={'Not Active'} />
+          </select>
+        </FormsContainer>
       </form>
+
+      <button>Save</button>
+      <button>Cancel</button>
     </section>
   );
 };
@@ -43,7 +76,17 @@ const Edit = () => {
 export default Edit;
 
 const Label = ({ label, id }) => (
-  <label className='text-gray-500 capitalize pb-1' htmlFor={id}>
+  <label className='text-gray-500 capitalize' htmlFor={id}>
     {label}
   </label>
+);
+
+const FormsContainer = ({ children }) => {
+  return <div className='flex flex-col gap-2 max-w-[340px]'>{children}</div>;
+};
+
+const Option = ({ value }) => (
+  <option className='font-sans capitalize' value={value}>
+    {value}
+  </option>
 );
