85c7611dbf432204a0c591afa7206be845454bdb
Tcodingg <princetlive@yahoo.com>
2022-11-24 22:41:08 -0500

    create options list data

---
+++ helpers/options.js
@@ -0,0 +1,14 @@
+export const rolesList = [
+  'software engineer',
+  'executive assistant',
+  'accountant',
+  'product manager',
+  'business analyst',
+  'sales representative',
+  'marketing specialist',
+  'human resource',
+];
+
+export const employeeTypeList = ['full time', 'part time'];
+
+export const employeeStatusList = ['active', 'not active'];
