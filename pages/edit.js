7e5f51d1919676b6be8d434e5a735ff09ffd4e54
Tcodingg <princetlive@yahoo.com>
2022-11-29 20:06:05 -0500

    redirect to the edit page when the edit button is dispatched

--- pages/edit.js
+++ pages/edit.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../components/Button/Button';
 import CustomSelection from '../components/CustomSelection/CustomSelection';
 import {
@@ -8,23 +9,48 @@ import {
 } from '../helpers/options';
 
 const Edit = () => {
-  const [selectedRole, setSelectedRole] = useState('');
-  const [selectedEmployeeType, setSelectedEmployeeType] = useState('');
-  const [selectedStatus, setSelectedStatus] = useState('');
+  const {
+    editSlice: { employeeData },
+  } = useSelector((state) => state);
+
+  const [name, setName] = useState(employeeData?.name);
+  const [selectedRole, setSelectedRole] = useState(employeeData.role);
+  const [selectedImage, setSelectedImage] = useState(employeeData?.image);
+  const [salary, setSalary] = useState(employeeData?.salary);
+  const [selectedEmployeeType, setSelectedEmployeeType] = useState(
+    employeeData?.typeEmployee
+  );
+  const [selectedStatus, setSelectedStatus] = useState(employeeData?.status);
+  const [newImage, setNewImage] = useState('');
+
+  const dispatch = useDispatch();
 
   return (
     <section className='max-w-4xl m-auto px-6 py-6'>
       <div>
         <div className='flex items-center '>
           <div className='border-4 h-40 w-40 rounded-full mt-3 overflow-hidden'>
-            <img src='/assets/images/employee1.png' alt='' />
+            <img
+              src={newImage ? URL.createObjectURL(newImage) : selectedImage}
+              alt='employee_image'
+            />
           </div>
           <div className='flex flex-col ml-6 gap-3'>
-            <Button
-              label={'change photo'}
-              textColor={'text-white'}
-              background={'bg-green-500'}
-            />
+            <label
+              className='max-w-[200px]'
+              htmlFor='
+            '
+            >
+              <input
+                className='max-w-[200px]'
+                type='file'
+                accept='image/png, image/gif, image/jpeg'
+                onChange={(e) => {
+                  setNewImage(e.target.files[0]);
+                  setSelectedImage(e.target.files[0]);
+                }}
+              />
+            </label>
             <Button
               label={'delete'}
               textColor={'text-black'}
@@ -39,7 +65,12 @@ const Edit = () => {
       <form className='mt-10 flex flex-col gap-6'>
         <FormsContainer>
           <Label label={'name'} />
-          <input className='border text-base px-2 py-3  ' type='text' />
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            className='border text-base px-2 py-3  '
+            type='text'
+          />
         </FormsContainer>
 
         <CustomSelection
@@ -66,11 +97,18 @@ const Edit = () => {
           <input
             className='border text-base px-2 py-3 appearance-none'
             type='text'
+            value={salary}
+            onChange={(e) => setSalary(e.target.value)}
           />
         </FormsContainer>
       </form>
       <div className='mt-10 flex gap-5'>
-        <Button label={'save'} background={'bg-white'} width={'w-40'} />
+        <Button
+          handleClick={() => dispatch({})}
+          label={'save'}
+          background={'bg-white'}
+          width={'w-40'}
+        />
         <Button
           label={'cancel'}
           background={'bg-red-400'}
--- redux/editSlice.js
+++ redux/editSlice.js
@@ -1,11 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
-const initialState = {};
+const initialState = {
+  employeeData: {
+    id: '',
+    image: '',
+    name: '',
+    role: '',
+    salary: null,
+    typeEmployee: '',
+  },
+};
 const editSlice = createSlice({
   name: 'edit',
   initialState,
   reducers: {
     editEmployee(state, action) {
-      state = action.payload;
+      // console.log(action.payload);
+      state.employeeData = action.payload;
     },
   },
 });
