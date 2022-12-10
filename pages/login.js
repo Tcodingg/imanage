dd80fa039f40ed6cf1c5874b6179a4a5ea7d4088
Tcodingg <princetlive@yahoo.com>
2022-12-10 18:42:00 -0500

    store the input onchange values in a state

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [input, setInput] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  };
+
   return (
     <section className='m-auto max-w-4xl h-[calc(100vh-4rem)] '>
       <div className='flex flex-col items-center justify-center h-full'>
@@ -9,18 +19,24 @@ const Login = () => {
           <div className='flex flex-col'>
             <label htmlFor='email'>Email:</label>
             <input
+              name='email'
               className='border py-3 px-1 outline-none rounded-sm'
               type='email'
+              onChange={handleChange}
+              value={input.email}
             />
           </div>
           <div className='flex flex-col'>
             <label htmlFor='email'>Password:</label>
             <input
+              onChange={handleChange}
+              value={input.password}
+              name='password'
               className='border py-3 px-1 outline-none rounded-sm'
               type='password'
             />
           </div>
-          <button className='bg-blue-400 py-3 text-white rounded-sm'>
+          <button className='bg-blue-500 py-3 text-white rounded-sm'>
             Submit
           </button>
         </form>
