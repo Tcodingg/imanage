5864bd4abef208778311b689860054dfe24c8d78
Tcodingg <princetlive@yahoo.com>
2022-12-11 21:49:40 -0500

    add logout api route. remove users cookie when requested

---
+++ pages/api/auth/logout.js
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { cookie, serialize } from 'cookie';
+
+export default async (req, res) => {
+  const { cookies } = req;
+
+  const jwt = cookies.get('token');
+
+  if (!jwt) {
+    return res.json({ message: 'You already have logged out!' });
+  } else {
+    const serialized = serialize('token', null, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development',
+      sameSite: 'strict',
+      maxAge: -1,
+      path: '/',
+    });
+
+    res.setHeader('Set-Cookie', serialized);
+    res.status(200).json({ message: 'Successfully logged out!' });
+  }
+};
