4f9d5211adabf775b506562b0e54e87811525427
Tcodingg <princetlive@yahoo.com>
2022-12-13 11:39:22 -0500

    create a form component and refactor the create page.

---
+++ components/Form.js
@@ -0,0 +1,181 @@
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Button from './Button/Button';
+import CustomSelection from './CustomSelection/CustomSelection';
+import {
+  employeeStatusList,
+  employeeTypeList,
+  rolesList,
+} from '../helpers/options';
+
+import { createEmployee, updateEmployee } from '../redux/employeesSlice';
+
+// refactoring
+//1.  have separate state both forms.
+//2.  conditionally check the type of action when the buttons are clicked
+
+const Form = ({ ...props }) => {
+  const {
+    editSlice: { employeeData },
+  } = useSelector((state) => state);
+
+  const {
+    action,
+    name,
+    setName,
+    selectedRole,
+    setSelectedRole,
+    selectedEmployeeType,
+    setSelectedEmployeeType,
+    selectedStatus,
+    setSelectedStatus,
+    salary,
+    setSalary,
+    selectedImage,
+    setSelectedImage,
+    newImage,
+    setNewImage,
+    error,
+    setError,
+    id,
+  } = props;
+
+  const dispatch = useDispatch();
+
+  let validInput =
+    name && selectedRole && salary && selectedEmployeeType && selectedStatus
+      ? true
+      : false;
+
+  const router = useRouter();
+  function handleClick(e) {
+    e.preventDefault();
+    if (validInput) {
+      const formData = new FormData();
+
+      formData.append('id', id);
+      formData.append('image', selectedImage);
+      formData.append('name', name);
+      formData.append('role', selectedRole);
+      formData.append('typeEmployee', selectedEmployeeType);
+      formData.append('status', selectedStatus);
+      formData.append('salary', salary);
+      !action === 'create'
+        ? dispatch(updateEmployee({ id, formData }))
+        : dispatch(createEmployee(formData));
+
+      router.push('/');
+      setError(false);
+    } else {
+      setError(true);
+    }
+  }
+
+  return (
+    <section className='max-w-4xl m-auto px-6 py-6'>
+      <div>
+        <div className='flex items-center '>
+          <div className='border-4 h-40 w-40 rounded-full mt-3 overflow-hidden'>
+            <img
+              src={newImage ? URL.createObjectURL(newImage) : selectedImage}
+              alt='employee_image'
+            />
+          </div>
+          <div className='flex flex-col ml-6 gap-3'>
+            <label
+              className='max-w-[200px]'
+              htmlFor='
+            '
+            >
+              <input
+                className='max-w-[200px]'
+                type='file'
+                accept='image/png, image/gif, image/jpeg'
+                onChange={(e) => {
+                  setNewImage(e.target.files[0]);
+                  setSelectedImage(e.target.files[0]);
+                }}
+              />
+            </label>
+          </div>
+        </div>
+        <p className='text-gray-500 mt-5 text-sm'>
+          Recommended size is 256 X 256 or leave it blank.
+        </p>
+      </div>
+      <form className='mt-10 flex flex-col gap-6'>
+        <FormsContainer>
+          <Label label={'name'} />
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            className='border text-base px-2 py-3  '
+            type='text'
+          />
+        </FormsContainer>
+
+        <CustomSelection
+          optionsList={rolesList}
+          selectedOption={selectedRole}
+          setSelectedOption={setSelectedRole}
+          label={'role'}
+        />
+        <CustomSelection
+          optionsList={employeeTypeList}
+          selectedOption={selectedEmployeeType}
+          setSelectedOption={setSelectedEmployeeType}
+          label={'type employee'}
+        />
+        <CustomSelection
+          optionsList={employeeStatusList}
+          selectedOption={selectedStatus}
+          setSelectedOption={setSelectedStatus}
+          label={'status'}
+        />
+
+        <FormsContainer>
+          <Label label={'salary'} />
+          <input
+            className='border text-base px-2 py-3 appearance-none'
+            type='text'
+            value={salary}
+            onChange={(e) => setSalary(e.target.value)}
+          />
+        </FormsContainer>
+      </form>
+      <div className='mt-10 flex gap-5'>
+        <Button
+          handleClick={handleClick}
+          label={action === 'create' ? 'create' : 'save'}
+          background={'bg-white'}
+          width={'w-40'}
+        />
+        <Button
+          handleClick={() => router.push('/')}
+          label={'cancel'}
+          background={'bg-red-400'}
+          width={'w-40'}
+          textColor={'text-white'}
+        />
+      </div>
+      {error && (
+        <small className='text-red-500 text-center'>
+          Please fill out all fields!{' '}
+        </small>
+      )}
+    </section>
+  );
+};
+
+export default Form;
+
+const Label = ({ label, id }) => (
+  <label className='text-gray-500 capitalize' htmlFor={id}>
+    {label}
+  </label>
+);
+
+const FormsContainer = ({ children }) => {
+  return <div className='flex flex-col gap-2 max-w-[340px]'>{children}</div>;
+};
--- pages/create.js
+++ pages/create.js
@@ -1,131 +1,37 @@
 import React, { useState } from 'react';
-import Button from '../components/Button/Button';
-import CustomSelection from '../components/CustomSelection/CustomSelection';
-import {
-  employeeStatusList,
-  employeeTypeList,
-  rolesList,
-} from '../helpers/options';
-import { AiOutlineUpload } from 'react-icons/ai';
-import { useDispatch } from 'react-redux';
-import { createEmployee } from '../redux/employeesSlice';
+import Form from '../components/Form';
 
 const Create = () => {
   const [selectedRole, setSelectedRole] = useState('');
   const [selectedEmployeeType, setSelectedEmployeeType] = useState('');
   const [selectedStatus, setSelectedStatus] = useState('');
   const [selectedImage, setSelectedImage] = useState('');
-  const [selectedSalary, setSelectedSalary] = useState();
-  const [selectedName, setSelectedName] = useState('');
-
-  const dispatch = useDispatch();
-
-  async function handleCreate(e) {
-    e.preventDefault();
-
-    const formData = new FormData();
-    formData.append('image', selectedImage);
-    formData.append('name', selectedName);
-    formData.append('role', selectedRole);
-    formData.append('typeEmployee', selectedEmployeeType);
-    formData.append('status', selectedStatus);
-    formData.append('salary', selectedSalary);
-    dispatch(createEmployee(formData));
-  }
+  const [salary, setSalary] = useState();
+  const [name, setName] = useState('');
+  const [newImage, setNewImage] = useState('');
+  const [error, setError] = useState(null);
 
   return (
-    <section className='max-w-4xl m-auto px-6 py-6'>
-      <div>
-        <div className='flex items-center '>
-          <label>
-            <input
-              onChange={(e) => setSelectedImage(e.target.files[0])}
-              className='w-full h-full'
-              type='file'
-            />
-          </label>
-          <div className='flex flex-col ml-6 gap-3'>
-            <Button
-              label={'change photo'}
-              textColor={'text-white'}
-              background={'bg-green-500'}
-            />
-            <Button
-              label={'delete'}
-              textColor={'text-black'}
-              background={'bg-white'}
-            />
-          </div>
-        </div>
-        <p className='text-gray-500 mt-5 text-sm'>
-          Recommended size is 256 X 256 or leave it blank.
-        </p>
-      </div>
-      <div className='mt-10 flex flex-col gap-6'>
-        <FormsContainer>
-          <Label label={'name'} />
-          <input
-            className='border text-base px-2 py-3  '
-            type='text'
-            onChange={(e) => setSelectedName(e.target.value)}
-          />
-        </FormsContainer>
-
-        <CustomSelection
-          optionsList={rolesList}
-          selectedOption={selectedRole}
-          setSelectedOption={setSelectedRole}
-          label={'role'}
-        />
-        <CustomSelection
-          optionsList={employeeTypeList}
-          selectedOption={selectedEmployeeType}
-          setSelectedOption={setSelectedEmployeeType}
-          label={'type employee'}
-        />
-        <CustomSelection
-          optionsList={employeeStatusList}
-          selectedOption={selectedStatus}
-          setSelectedOption={setSelectedStatus}
-          label={'status'}
-          onChange={(e) => setSelectedStatus(e.target.value)}
-        />
-
-        <FormsContainer>
-          <Label label={'salary'} />
-          <input
-            className='border text-base px-2 py-3 appearance-none'
-            type='text'
-            onChange={(e) => setSelectedSalary(e.target.value)}
-          />
-        </FormsContainer>
-      </div>
-      <div className='mt-10 flex gap-5'>
-        <Button
-          handleClick={handleCreate}
-          label={'create'}
-          background={'bg-white'}
-          width={'w-40'}
-        />
-        <Button
-          label={'cancel'}
-          background={'bg-red-400'}
-          width={'w-40'}
-          textColor={'text-white'}
-        />
-      </div>
-    </section>
+    <Form
+      action={'create'}
+      name={name}
+      setName={setName}
+      selectedRole={selectedRole}
+      setSelectedRole={setSelectedRole}
+      selectedEmployeeType={selectedEmployeeType}
+      setSelectedEmployeeType={setSelectedEmployeeType}
+      selectedStatus={selectedStatus}
+      setSelectedStatus={setSelectedStatus}
+      salary={salary}
+      setSalary={setSalary}
+      selectedImage={selectedImage}
+      setSelectedImage={setSelectedImage}
+      newImage={newImage}
+      setNewImage={setNewImage}
+      error={error}
+      setError={setError}
+    />
   );
 };
 
 export default Create;
-
-const Label = ({ label, id }) => (
-  <label className='text-gray-500 capitalize' htmlFor={id}>
-    {label}
-  </label>
-);
-
-const FormsContainer = ({ children }) => {
-  return <div className='flex flex-col gap-2 max-w-[340px]'>{children}</div>;
-};
