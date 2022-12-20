f707945e5adcbdb734ec431df133b3e13c638c72
Tcodingg <princetlive@yahoo.com>
2022-12-20 00:40:37 -0500

    sort employees when the table header is clicked

--- components/Table/Table.js
+++ components/Table/Table.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getEmployees } from '../../redux/employeesSlice';
 import TableData from '../TableData/TableData';
@@ -13,14 +13,39 @@ const Table = ({ search }) => {
     dispatch(getEmployees());
   }, [dispatch]);
 
+  const [data, setData] = useState([...employees]);
+  const [order, setOrder] = useState('ascending');
+
+  function handleSort(key) {
+    if (order === 'ascending') {
+      const sort = [...data].sort((a, b) => (a[key] > b[key] ? 1 : -1));
+      setData(sort);
+      setOrder('descending');
+    }
+    if (order === 'descending') {
+      const sort = [...data].sort((a, b) => (a[key] < b[key] ? 1 : -1));
+      setOrder('ascending');
+      setData(sort);
+    }
+  }
+
   return (
     <section className='relative ml-auto mr-auto max-w-4xl min-h-[calc(100vh-4rem)] bg-gray-100'>
       <table className='text-left border-collapse border-spacing-0 w-full '>
         <thead className=''>
           <tr className='flex justify-between m-0 px-3 h-[3.5rem]'>
-            <TableHeader title='employee' />
-            <TableHeader title='salary' />
-            <TableHeader title='status' />
+            <TableHeader
+              title='employee'
+              handleSort={() => handleSort('name')}
+            />
+            <TableHeader
+              title='salary'
+              handleSort={() => handleSort('salary')}
+            />
+            <TableHeader
+              title='status'
+              handleSort={() => handleSort('status')}
+            />
             <TableHeader title='manage' align='text-center' />
           </tr>
         </thead>
@@ -28,7 +53,7 @@ const Table = ({ search }) => {
           className='h-[calc(100vh-10.5rem)] overflow-y-scroll block
         '
         >
-          {employees
+          {data
             ?.filter(({ name }) =>
               name.toString().toLowerCase().includes(search.toLowerCase())
             )
@@ -52,9 +77,9 @@ const Table = ({ search }) => {
 
 export default Table;
 
-function TableHeader({ title }) {
+function TableHeader({ title, handleSort }) {
   return (
-    <th className='flex-1 m-0 '>
+    <th className='flex-1 m-0 hover:cursor-pointer' onClick={handleSort}>
       <p className={'py-5  uppercase text-xs font-medium text-gray-600 '}>
         {title}
       </p>
