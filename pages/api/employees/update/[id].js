f6e2b9222a7b42be545567a071255823c85675e3
Tcodingg <princetlive@yahoo.com>
2022-12-25 17:14:07 -0500

    store the updated data in sql db

--- pages/api/employees/update/[id].js
+++ pages/api/employees/update/[id].js
@@ -1,7 +1,7 @@
 import nextConnect from 'next-connect';
 import multer from 'multer';
-import employees from '../../../../models/employees';
 import fs from 'fs';
+import db from '../../../../config/db';
 
 const upload = multer({
   storage: multer.diskStorage({
@@ -30,7 +30,7 @@ handler.use(upload.single('image'));
 handler.patch(async (req, res) => {
   const { method } = req;
   if (method === 'PATCH') {
-    const id = req.query.id;
+    const id = Number(req.query?.id);
     const name = req.body.name;
     const image = req.file?.filename;
     const role = req.body.role;
@@ -38,48 +38,25 @@ handler.patch(async (req, res) => {
     const status = req.body.status;
     const salary = req.body.salary;
 
-    const updateEmployee = {
-      name,
-      image,
-      role,
-      typeEmployee,
-      status,
-      salary,
-    };
-
     let result;
     try {
-      const employee = await employees.findById(id);
+      let employee = await getEmployee(id);
+
       if (image) {
         let imagePath = `public/assets/images/employees/${employee.image}`;
         fs.unlinkSync(imagePath);
 
-        result = await employees.findByIdAndUpdate(
-          id,
-          {
-            name,
-            image,
-            role,
-            typeEmployee,
-            status,
-            salary,
-          },
+        let sqlUpdate = `UPDATE Employees SET name=?, image=?, salary=?, role=?, status=?, typeEmployee=? WHERE _id=?`;
+        let values = [name, image, salary, role, status, typeEmployee, id];
 
-          { new: true }
-        );
+        const [results] = await db.query(sqlUpdate, values);
+        result = await getEmployee(id);
       } else {
-        result = await employees.findByIdAndUpdate(
-          id,
-          {
-            image: employee.image,
-            name,
-            role,
-            typeEmployee,
-            status,
-            salary,
-          },
-          { new: true }
-        );
+        let sqlUpdate = `UPDATE Employees SET name=?, salary=?, role=?, status=?, typeEmployee=? WHERE _id=?`;
+        let values = [name, salary, role, status, typeEmployee, id];
+
+        const [results] = await db.query(sqlUpdate, values);
+        result = await getEmployee(id);
       }
 
       res.status(200).json(result);
@@ -94,4 +71,10 @@ export const config = {
   api: {
     bodyParser: false, // Disallow body parsing, consume as stream
   },
+};
+
+const getEmployee = async (id) => {
+  let query = 'SELECT * FROM Employees WHERE _id=?';
+  const [result] = await db.query(query, [id]);
+  return result[0];
 };
