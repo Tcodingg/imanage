ab3b6e04da7db5a6f3ebeca075efecbd6756b403
Tcodingg <princetlive@yahoo.com>
2022-12-29 12:44:43 -0500

    sort data only when the name of employees or the arrow is clicked

--- components/TableHeader.js
+++ components/TableHeader.js
@@ -2,26 +2,26 @@ import React from 'react';
 import { HiOutlineSortAscending } from 'react-icons/hi';
 function TableHeader({ title, handleSort, order, col }) {
   return (
-    <th
-      className={`flex-1 m-0 ${
-        title !== 'action' ? 'hover:cursor-pointer' : ''
-      } `}
-      onClick={handleSort}
-    >
-      <p
-        className={
-          'py-5 flex items-center gap-4 uppercase text-xs font-medium text-gray-600 '
-        }
+    <th className={'flex-1 m-0 w-1/4 '}>
+      <div
+        onClick={handleSort}
+        className={`w-fit ${title !== 'action' ? 'hover:cursor-pointer' : ''}`}
       >
-        {title}
-        <span
-          className={`text-lg ${
-            col === title ? 'text-gray-800' : 'text-gray-400'
-          } ${col === title && order === 'descending' ? 'rotate-180' : ''}`}
+        <p
+          className={
+            'py-5 flex items-center gap-4 uppercase text-xs font-medium text-gray-600 '
+          }
         >
-          {title !== 'action' && <HiOutlineSortAscending />}
-        </span>
-      </p>
+          {title}
+          <span
+            className={`text-lg ${
+              col === title ? 'text-gray-800' : 'text-gray-400'
+            } ${col === title && order === 'descending' ? 'rotate-180' : ''}`}
+          >
+            {title !== 'action' && <HiOutlineSortAscending />}
+          </span>
+        </p>
+      </div>
     </th>
   );
 }
